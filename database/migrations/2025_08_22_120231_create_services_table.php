14b8bfec6326e64a4ded7c3ba4c8919b34560e74
NurCholisss <nurc11700@gmail.com>
2025-08-23 06:05:04 +0700

    migration

--- database/migrations/2025_08_22_120231_create_services_table.php
+++ database/migrations/2025_08_22_120231_create_services_table.php
@@ -11,14 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('services', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->enum('role', ['admin', 'customer'])->default('customer');
-            $table->string('phone')->nullable();
-            $table->text('address')->nullable();
+            $table->text('description')->nullable();
+            $table->decimal('price', 10, 2);
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
@@ -28,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('services');
     }
 };
